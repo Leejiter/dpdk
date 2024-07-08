Leejiter/dpdk

commit bf4416016c66407f6c4c3fc5ee90cf49427fec9f

net/nfp: split out flow item check logic
Split out the flow item check logic.

Signed-off-by: Chaoyong He <chaoyong.he@corigine.com>
Reviewed-by: Long Wu <long.wu@corigine.com>
Reviewed-by: Peng Zhang <peng.zhang@corigine.com>

drivers/net/nfp/flower/nfp_flower_flow.c
@@ -820,7 +820,110 @@ struct nfp_item_calculate_param {
 	struct nfp_item_flag *flag;
 };
 
+typedef int (*nfp_flow_key_check_item_fn)(struct nfp_item_calculate_param *param);
 typedef int (*nfp_flow_key_calculate_item_fn)(struct nfp_item_calculate_param *param);
+
+static int
+nfp_flow_item_check_port(struct nfp_item_calculate_param *param)
+{
+	const struct rte_flow_item_port_id *port_id;
+
+	port_id = param->item->spec;
+	if (port_id == NULL || port_id->id >= RTE_MAX_ETHPORTS)
+		return -ERANGE;
+
+	return 0;
+}
+
+static int
+nfp_flow_item_check_ipv4(struct nfp_item_calculate_param *param)
+{
+	if (!param->flag->outer_ip4_flag)
+		param->flag->outer_ip4_flag = true;
+
+	return 0;
+}
+
+static int
+nfp_flow_item_check_ipv6(struct nfp_item_calculate_param *param)
+{
+	if (!param->flag->outer_ip6_flag)
+		param->flag->outer_ip6_flag = true;
+
+	return 0;
+}
+
+static int
+nfp_flow_item_check_vxlan(struct nfp_item_calculate_param *param)
+{
+	if (!param->flag->outer_ip4_flag && !param->flag->outer_ip6_flag) {
+		PMD_DRV_LOG(ERR, "No outer IP layer for VXLAN tunnel.");
+		return -EINVAL;
+	}
+
+	return 0;
+}
+
+static int
+nfp_flow_item_check_geneve(struct nfp_item_calculate_param *param)
+{
+	if (!param->flag->outer_ip4_flag && !param->flag->outer_ip6_flag) {
+		PMD_DRV_LOG(ERR, "No outer IP layer for GENEVE tunnel.");
+		return -EINVAL;
+	}
+
+	return 0;
+}
+
+static int
+nfp_flow_item_check_gre(struct nfp_item_calculate_param *param)
+{
+	if (!param->flag->outer_ip4_flag && !param->flag->outer_ip6_flag) {
+		PMD_DRV_LOG(ERR, "No outer IP layer for GRE tunnel.");
+		return -EINVAL;
+	}
+
+	return 0;
+}
+
+static nfp_flow_key_check_item_fn check_item_fns[] = {
+	[RTE_FLOW_ITEM_TYPE_PORT_ID]         = nfp_flow_item_check_port,
+	[RTE_FLOW_ITEM_TYPE_IPV4]            = nfp_flow_item_check_ipv4,
+	[RTE_FLOW_ITEM_TYPE_IPV6]            = nfp_flow_item_check_ipv6,
+	[RTE_FLOW_ITEM_TYPE_VXLAN]           = nfp_flow_item_check_vxlan,
+	[RTE_FLOW_ITEM_TYPE_GENEVE]          = nfp_flow_item_check_geneve,
+	[RTE_FLOW_ITEM_TYPE_GRE]             = nfp_flow_item_check_gre,
+};
+
+static int
+nfp_flow_key_layers_check_items(const struct rte_flow_item items[])
+{
+	int ret;
+	struct nfp_item_flag flag = {};
+	const struct rte_flow_item *item;
+	struct nfp_item_calculate_param param = {
+		.flag = &flag,
+	};
+
+	for (item = items; item->type != RTE_FLOW_ITEM_TYPE_END; ++item) {
+		if (item->type >= RTE_DIM(check_item_fns)) {
+			PMD_DRV_LOG(ERR, "Flow item %d unsupported", item->type);
+			return -ERANGE;
+		}
+
+		if (check_item_fns[item->type] == NULL)
+			continue;
+
+		param.item = item;
+		ret = check_item_fns[item->type](&param);
+		if (ret != 0) {
+			PMD_DRV_LOG(ERR, "Flow item %d check fail", item->type);
+			return ret;
+		}
+	}
+
+	return 0;
+}
 
 static int
 nfp_flow_item_calculate_stub(struct nfp_item_calculate_param *param __rte_unused)
@@ -1489,6 +1592,12 @@ nfp_flow_key_layers_calculate(const struct rte_flow_item items[],
 	key_ls->port = ~0;
 	key_ls->vlan = 0;
 	key_ls->tun_type = NFP_FL_TUN_NONE;
+
+	ret = nfp_flow_key_layers_check_items(items);
+	if (ret != 0) {
+		PMD_DRV_LOG(ERR, "flow items check failed");
+		return ret;
+	}
 
 	ret = nfp_flow_key_layers_calculate_items(items, key_ls);
 	if (ret != 0) {

commit 415fa93d81ec59b7fd2ae3b38212478c5e3ecf1c

net/nfp: fix port action core dump
The app will be destroyed if add a port flow action which its
destination port id is invalid.

Add a check to avoid the problem.

Fixes: bf4416016c66 ("net/nfp: split out flow item check logic")
Fixes: 18c0ba1d69a8 ("net/nfp: split out flow action check logic")

Signed-off-by: Long Wu <long.wu@corigine.com>
Reviewed-by: Chaoyong He <chaoyong.he@corigine.com>

drivers/net/nfp/flower/nfp_flower_flow.c
@@ -977,7 +977,7 @@ nfp_flow_item_check_port(struct nfp_item_calculate_param *param)
 	const struct rte_flow_item_port_id *port_id;
 
 	port_id = param->item->spec;
-	if (port_id == NULL || port_id->id >= RTE_MAX_ETHPORTS)
+	if (port_id == NULL || rte_eth_dev_is_valid_port(port_id->id) == 0)
 		return -ERANGE;
 
 	return 0;
@@ -1302,7 +1302,7 @@ nfp_flow_action_check_port(struct nfp_action_calculate_param *param)
 		port_id = action_port_id->id;
 	}
 
-	if (port_id >= RTE_MAX_ETHPORTS)
+	if (rte_eth_dev_is_valid_port(port_id) == 0)
 		return -ERANGE;
 
 	return 0;